2f9b27609173c1082e63ce61d04821b6831e02d1
wpem3123 <wpem3123@naver.com>
2022-08-30 13:27:29 +0900

    디스페처 서블릿 2번째

--- src/main/java/site/metacoding/ds/UserController.java
+++ src/main/java/site/metacoding/ds/UserController.java
@@ -3,13 +3,19 @@ package site.metacoding.ds;
 public class UserController {
 	
 	
-	
+	@RequestMapping("/join")
 	public void join() {
 		System.out.println("join 호출됨");
 	}
 	
+	@RequestMapping("/login")
 	public void login() {
 		System.out.println("login 호출됨");
+	}
+	
+	@RequestMapping("/joinForm")
+	public void joinForm() {
+		System.out.println("joinForm 호출됨");
 	}
 	
 }
